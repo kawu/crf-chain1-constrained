e23f17916f5cfae76d2ba3b906c23aa2cfe3addb
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-11-08 10:54:00 +0100

    Current snapshot

--- src/Data/CRF/Chain1/Constrained/Dataset/Internal.hs
+++ src/Data/CRF/Chain1/Constrained/Dataset/Internal.hs
@@ -126,6 +126,12 @@ type Ys = V.Vector Y
 --
 -- Naming proposals:
 -- * lattice
+-- * word-lattice       <- może za bardzo kojarzy się z czymś innym?
+--                         ale z drugiej strony, chyba kojarz się
+--                         właśnie z tym co trzeba, ew. sugeruje trochę
+--                         inną funkcjonalność (jaką?) niż ja zamierzam
+--                         udostępnić.
+-- * word-dag
 -- * edge-dag
 --
 -- First of all, nodes represent division places of the input text
@@ -144,6 +150,9 @@ type Ys = V.Vector Y
 -- It should be possible to make something like a context-sensitive
 -- fmap, which will make it possible to:
 -- * Translate individual edge values to feature vectors,
+--   WARNING: there are several strategies of how observations for the,
+--   previous edge (for example) are determined, since there may be
+--   several candidates for a previous edge!
 -- * Translate edge values to forward/backward probabilities.
 --
 -- Misc operations:
@@ -152,3 +161,17 @@ type Ys = V.Vector Y
 --   a sequence of morphosyntactic tags refering to a particular path
 --   in the DAG), or more general:
 -- * Join DAG with a DAG.
+--
+-- Other considerations:
+-- * Do we want to represent spaces on the level of word lattices?
+--   Not necessarily.  On the other hand, it should be possible
+--   to store a word-with-spaces in an edge -- why not? -- and
+--   restore the original sentence from the lattice.  Wouldn't
+--   that be nice?
+--   Since node represents a position between two segments, it can
+--   be also explicitly stated if there is a space between these two
+--   segments, and what kind of space is that.  Yep, it makes sense!
+-- * Potential morphosyntactic analyses of a sentence can be represented
+--   as a DAG.  The same goes for chosen interpretations -- they can also
+--   be represented as a DAG with potentials (probabilities?) assigned
+--   to each label on every edge.
